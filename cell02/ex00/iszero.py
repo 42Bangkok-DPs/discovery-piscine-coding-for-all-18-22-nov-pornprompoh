1d9c6ac4afdae02223b2d20134f9cc3dd90b2df5
pornprompoh <poh17449@gmail.com>
2024-11-21 05:00:44 +0000

    test

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+a = int(input("Enter number: "))
+if a < 0:
+    print("This number is different from zero.")
+elif a == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
+
